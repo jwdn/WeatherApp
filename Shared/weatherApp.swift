172c74f3232fcd1a63d0269c3aa1d1a6f0f3e36a
Jon Wadden <jon@wadden.io>
2021-06-20 14:42:12 -0600

    Initial Commit

---
+++ Shared/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  Shared
+//
+//  Created by Jon Wadden on 2021-06-20.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
